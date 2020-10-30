fdb1835189ac55d403a5ea366c6d5d516fcff0b9
tinkerton <jonas@Tinkerbook-Pro.localdomain>
2020-10-30 18:08:26 +0100

    Updates

---
+++ precache-manifest.d1cd52bbdbd80441735b85f61f4b39c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e839e1a11003d40a5c08a60500d9101",
+    "url": "/chas_magicthegathering/index.html"
+  },
+  {
+    "revision": "bc5ccdc5136d192c4366",
+    "url": "/chas_magicthegathering/static/css/main.85bb3c47.chunk.css"
+  },
+  {
+    "revision": "16f2fdc46ce1ef4af9bb",
+    "url": "/chas_magicthegathering/static/js/2.0acecec1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/chas_magicthegathering/static/js/2.0acecec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5ccdc5136d192c4366",
+    "url": "/chas_magicthegathering/static/js/main.42c123a8.chunk.js"
+  },
+  {
+    "revision": "8f553e7d292a4378979d",
+    "url": "/chas_magicthegathering/static/js/runtime-main.661c4a21.js"
+  }
+]);
