dc02e8e9392e11d8bc850e827d39528c939c72c1
tinkerton <jonas@Tinkerbook-Pro.localdomain>
2020-11-03 08:11:21 +0100

    Updates

---
+++ precache-manifest.ec182330b64b712c407c0f81c60ed46f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3f458dac1e1d67070dbfddb51c00a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f33795c21aad87a13524",
+    "url": "/static/css/main.85bb3c47.chunk.css"
+  },
+  {
+    "revision": "16f2fdc46ce1ef4af9bb",
+    "url": "/static/js/2.0acecec1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0acecec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33795c21aad87a13524",
+    "url": "/static/js/main.1103c88f.chunk.js"
+  },
+  {
+    "revision": "88c738b370fd58e8cafa",
+    "url": "/static/js/runtime-main.60e25a90.js"
+  }
+]);
